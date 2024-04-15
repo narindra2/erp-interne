b52b58471d56af0eb7d654d317d47d90c0328814
narindra2 <narindrandimby2@gmail>
2024-04-15 16:28:50 +0300

    migration-erp-ok

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -9,17 +9,17 @@ class Menu extends Model
 {
     use HasFactory;
     protected $with = ['submenu'];
-    
+
     public static $USER_ALLOWED_PART_ACCESS = [
         "suivi_testeur" => [
             100121, 100043, 100036, 100082, 100155,
-            100139,100167, 100109,100042, 100047,
-            100053,100057,100090,100055,100110,100080,100134,100116,100130,
+            100139, 100167, 100109, 100042, 100047,
+            100053, 100057, 100090, 100055, 100110, 100080, 100134, 100116, 100130,
         ],
-        "debug_tools" => [100043],
+        "debug_tools" => [ /*100043 */],
         "complement_hours" => [100043]
     ];
-    public function submenu() 
+    public function submenu()
     {
         return $this->hasMany(Submenu::class, 'menus_id');
     }
